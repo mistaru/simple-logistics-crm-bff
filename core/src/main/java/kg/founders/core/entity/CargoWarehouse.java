a5e2bf51c84a5641ca7e2f45dad0ec9509e1b3ed
btatenov <btatenov@devcats.kg>
2025-03-02 17:41:51 +0600

    CargoWarehouse связь Cargo + Warehouse

---
+++ core/src/main/java/kg/founders/core/entity/CargoWarehouse.java
@@ -0,0 +1,30 @@
+package kg.founders.core.entity;
+
+import kg.founders.core.util.SqlTable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = Cargo.TABLE_NAME)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class CargoWarehouse {
+
+    @SqlTable
+    public static final String TABLE_NAME = "CARGO_WAREHOUSE";
+    public static final String SEQ_NAME = TABLE_NAME + "_SEQ";
+
+    @Id
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME, allocationSize = 1)
+    @GeneratedValue(generator = SEQ_NAME)
+    Long id;
+    Long cargoId;
+    Long warehouseId;
+}
