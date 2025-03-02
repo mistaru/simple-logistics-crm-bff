04bf0f83e6cf6eb4dceb8de2a78c0b146b0ed0b1
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2025-03-02 18:20:00 +0600

    удалил в конфигах схему logistic, переписал скрипт 001_ADMIN.sql на диалект PostgreSQL

--- core/src/main/resources/db/INITIAL_DB_CREATION.sql
+++ core/src/main/resources/db/INITIAL_DB_CREATION.sql
@@ -1,3 +1,4 @@
+-- ONLY 3 STEPS REQUIRED !!!
 -- 1. Create the database (if it doesn't exist)
 CREATE DATABASE crm_bff WITH OWNER = postgres
          ENCODING = 'UTF8'
@@ -11,10 +12,10 @@ CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres';
 GRANT ALL PRIVILEGES ON DATABASE crm_bff TO postgres;
 
 -- 4. Connect to the crm_bff database using psql
-\c crm_bff
-
--- 5. Create the logistic schema
-CREATE SCHEMA IF NOT EXISTS logistic AUTHORIZATION postgres;  -- postgres owns the schema
-
--- 6. Grant privileges on the schema to the user (Important!)
-GRANT ALL PRIVILEGES ON SCHEMA logistic TO postgres;
+-- \c crm_bff
+--
+-- -- 5. Create the logistic schema
+-- CREATE SCHEMA IF NOT EXISTS logistic AUTHORIZATION postgres;  -- postgres owns the schema
+--
+-- -- 6. Grant privileges on the schema to the user (Important!)
+-- GRANT ALL PRIVILEGES ON SCHEMA logistic TO postgres;
