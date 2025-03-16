f939975628ba2c4786c8f13da072476daa2def8e
Argen Kasymov <arkasymov@devcats.kg>
2025-03-16 23:22:45 +0600

    фикс багов круд ролей

--- bff/src/main/java/kg/founders/bff/controller/role/RoleControllerRest.java
+++ bff/src/main/java/kg/founders/bff/controller/role/RoleControllerRest.java
@@ -2,9 +2,11 @@ package kg.founders.bff.controller.role;
 
 import kg.founders.bff.config.settings.perms.PermissionValidation;
 import kg.founders.core.enums.permission.AccessType;
+import kg.founders.core.enums.permission.PermissionType;
 import kg.founders.core.model.auth.role.LogisticRoleModel;
 import kg.founders.core.services.auth.role.RoleService;
 import kg.founders.core.settings.security.permission.annotation.HasAccess;
+import kg.founders.core.settings.security.permission.annotation.HasPermission;
 import kg.founders.core.settings.security.permission.annotation.ManualPermissionControl;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -16,7 +18,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/role")
-//@HasPermission(PermissionType.ROLE)
+@HasPermission(PermissionType.ROLE)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RoleControllerRest {
 
@@ -27,7 +29,6 @@ public class RoleControllerRest {
     public List<LogisticRoleModel> list() {
         return roleService.listAllAsModel();
     }
-
 
     @PostMapping
     @HasAccess({AccessType.CREATE, AccessType.UPDATE})
