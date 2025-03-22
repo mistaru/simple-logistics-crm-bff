2a06dba4062cb80368c8d9b4943e25215c313eb8
Argen Kasymov <arkasymov@devcats.kg>
2025-03-22 17:21:01 +0600

    добавил метод получения всех клиентов

--- bff/src/main/java/kg/founders/bff/controller/ClientControllerRest.java
+++ bff/src/main/java/kg/founders/bff/controller/ClientControllerRest.java
@@ -2,14 +2,18 @@ package kg.founders.bff.controller;
 
 import kg.founders.core.enums.permission.PermissionType;
 import kg.founders.core.model.ClientModel;
+import kg.founders.core.model.dictionaries.CountryDictModel;
 import kg.founders.core.services.ClientService;
 import kg.founders.core.settings.security.permission.annotation.HasPermission;
+import kg.founders.core.settings.security.permission.annotation.ManualPermissionControl;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PRIVATE;
@@ -22,6 +26,12 @@ import static lombok.AccessLevel.PRIVATE;
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 public class ClientControllerRest {
     ClientService clientService;
+
+    @GetMapping("/all")
+    @ManualPermissionControl
+    public List<ClientModel> getAll() {
+        return clientService.getAll();
+    }
 
     @GetMapping
     public Page<ClientModel> get(Pageable pageable) {
--- core/src/main/java/kg/founders/core/services/impl/ClientServiceImpl.java
+++ core/src/main/java/kg/founders/core/services/impl/ClientServiceImpl.java
@@ -3,6 +3,7 @@ package kg.founders.core.services.impl;
 import kg.founders.core.converter.ClientConverter;
 import kg.founders.core.entity.Client;
 import kg.founders.core.model.ClientModel;
+import kg.founders.core.model.dictionaries.CountryDictModel;
 import kg.founders.core.repo.ClientRepo;
 import kg.founders.core.services.ClientService;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,9 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PRIVATE;
@@ -38,5 +42,10 @@ public class ClientServiceImpl implements ClientService {
         Client client = clientConverter.convertFromModel(clientModel);
         clientRepo.save(client);
         return clientConverter.convertFromEntity(client);
+    }
+
+    @Override
+    public List<ClientModel> getAll() {
+        return clientRepo.findAll().stream().map(clientConverter::convertFromEntity).collect(Collectors.toList());
     }
 }
